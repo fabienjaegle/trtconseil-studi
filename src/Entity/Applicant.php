0f5ca6662217242037a62e8c331dd78cdabfe236
Fabien Jaegle <fjaegle@divalto.com>
2022-08-11 09:57:07 +0200

    Add Applicant entity

---
+++ src/Entity/Applicant.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ApplicantRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ApplicantRepository::class)]
+class Applicant
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $cv = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $userlink = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCv(): ?string
+    {
+        return $this->cv;
+    }
+
+    public function setCv(?string $cv): self
+    {
+        $this->cv = $cv;
+
+        return $this;
+    }
+
+    public function getUserlink(): ?User
+    {
+        return $this->userlink;
+    }
+
+    public function setUserlink(?User $userlink): self
+    {
+        $this->userlink = $userlink;
+
+        return $this;
+    }
+}
