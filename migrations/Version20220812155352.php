bc21fe89524ae81513d50a64db90fb8912abdc1c
Fabien Jaegle <fjaegle@divalto.com>
2022-08-14 09:50:38 +0200

    Add Consultant migration

--- migrations/Version20220812155352.php
+++ migrations/Version20220812155352.php
@@ -20,11 +20,12 @@ final class Version20220812155352 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE "user" ADD role VARCHAR(50) NOT NULL');
+        $this->addSql('ALTER TABLE "user" ADD role VARCHAR(50)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE "user" DROP role');
     }
 }
