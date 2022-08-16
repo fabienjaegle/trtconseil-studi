c916718d80fd83f5884ffc6485834eb962742947
Fabien Jaegle <fjaegle@divalto.com>
2022-08-16 12:12:08 +0200

    Job offer approval or rejection

--- src/Controller/Consultant/ConsultantDashboardController.php
+++ src/Controller/Consultant/ConsultantDashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Consultant;
 
 use App\Repository\CandidateRepository;
 use App\Repository\RecruiterRepository;
+use App\Repository\JobOfferRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,16 +15,17 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 class ConsultantDashboardController extends AbstractController
 {
     #[Route('/dashboard/consultant', name: 'app_dashboard_consultant_index')]
-    public function index(CandidateRepository $candidateRepository, RecruiterRepository $recruiterRepository): Response
+    public function index(CandidateRepository $candidateRepository, RecruiterRepository $recruiterRepository, JobOfferRepository $jobOfferRepository): Response
     {
         return $this->render('consultant/index.html.twig', [
             'candidates' => $candidateRepository->findBy(['isValidated' => null]),
-            'recruiters' => $recruiterRepository->findBy(['isValidated' => null])
+            'recruiters' => $recruiterRepository->findBy(['isValidated' => null]),
+            'joboffers' => $jobOfferRepository->findBy(['isValidated' => null])
         ]);
     }
 
     #[Route('/dashboard/consultant/approuveCandidate/{id}', name: 'app_dashboard_consultant_approuve_candidate', methods: ['GET'], requirements: ['id' => '\d+'])]
-    public function approuveCandidate(CandidateRepository $candidateRepository, RecruiterRepository $recruiterRepository, int $id, EntityManagerInterface $entityManager): Response
+    public function approuveCandidate(CandidateRepository $candidateRepository, int $id, EntityManagerInterface $entityManager): Response
     {
         $candidate = $candidateRepository->find($id);
 
@@ -36,14 +38,11 @@ class ConsultantDashboardController extends AbstractController
             'Le compte candidat a été approuvé.'
         );
 
-        return $this->render('consultant/index.html.twig', [
-            'candidates' => $candidateRepository->findBy(['isValidated' => null]),
-            'recruiters' => $recruiterRepository->findBy(['isValidated' => null])
-        ]);
+        return $this->redirectToRoute('app_dashboard_consultant_index');
     }
 
     #[Route('/dashboard/consultant/rejectCandidate/{id}', name: 'app_dashboard_consultant_reject_candidate', methods: ['GET'], requirements: ['id' => '\d+'])]
-    public function rejectCandidate(CandidateRepository $candidateRepository, RecruiterRepository $recruiterRepository, int $id, EntityManagerInterface $entityManager): Response
+    public function rejectCandidate(CandidateRepository $candidateRepository, int $id, EntityManagerInterface $entityManager): Response
     {
         $candidate = $candidateRepository->find($id);
 
@@ -56,14 +55,11 @@ class ConsultantDashboardController extends AbstractController
             'Le compte candidat a été rejeté.'
         );
 
-        return $this->render('consultant/index.html.twig', [
-            'candidates' => $candidateRepository->findBy(['isValidated' => null]),
-            'recruiters' => $recruiterRepository->findBy(['isValidated' => null])
-        ]);
+        return $this->redirectToRoute('app_dashboard_consultant_index');
     }
 
     #[Route('/dashboard/consultant/approuveRecruiter/{id}', name: 'app_dashboard_consultant_approuve_recruiter', methods: ['GET'], requirements: ['id' => '\d+'])]
-    public function approuveRecruiter(CandidateRepository $candidateRepository, RecruiterRepository $recruiterRepository, int $id, EntityManagerInterface $entityManager): Response
+    public function approuveRecruiter(RecruiterRepository $recruiterRepository, int $id, EntityManagerInterface $entityManager): Response
     {
         $recruiter = $recruiterRepository->find($id);
 
@@ -76,14 +72,11 @@ class ConsultantDashboardController extends AbstractController
             'Le compte recruteur a été approuvé.'
         );
 
-        return $this->render('consultant/index.html.twig', [
-            'candidates' => $candidateRepository->findBy(['isValidated' => null]),
-            'recruiters' => $recruiterRepository->findBy(['isValidated' => null])
-        ]);
+        return $this->redirectToRoute('app_dashboard_consultant_index');
     }
 
     #[Route('/dashboard/consultant/rejectRecruiter/{id}', name: 'app_dashboard_consultant_reject_recruiter', methods: ['GET'], requirements: ['id' => '\d+'])]
-    public function rejectRecruiter(CandidateRepository $candidateRepository, RecruiterRepository $recruiterRepository, int $id, EntityManagerInterface $entityManager): Response
+    public function rejectRecruiter(RecruiterRepository $recruiterRepository, int $id, EntityManagerInterface $entityManager): Response
     {
         $recruiter = $recruiterRepository->find($id);
 
@@ -93,12 +86,43 @@ class ConsultantDashboardController extends AbstractController
 
         $this->addFlash(
             'error',
-            'Le compte candidat a été rejeté.'
+            'Le compte recruteur a été rejeté.'
         );
 
-        return $this->render('consultant/index.html.twig', [
-            'candidates' => $candidateRepository->findBy(['isValidated' => null]),
-            'recruiters' => $recruiterRepository->findBy(['isValidated' => null])
-        ]);
+        return $this->redirectToRoute('app_dashboard_consultant_index');
+    }
+
+    #[Route('/dashboard/consultant/approuveOffer/{id}', name: 'app_dashboard_consultant_approuve_offer', methods: ['GET'], requirements: ['id' => '\d+'])]
+    public function approuveOffer(JobOfferRepository $jobOfferRepository, int $id, EntityManagerInterface $entityManager): Response
+    {
+        $jobOffer = $jobOfferRepository->find($id);
+
+        $jobOffer->setIsValidated(true);
+        $entityManager->persist($jobOffer);
+        $entityManager->flush();
+
+        $this->addFlash(
+            'success',
+            'L\'annonce a été approuvée.'
+        );
+
+        return $this->redirectToRoute('app_dashboard_consultant_index');
+    }
+
+    #[Route('/dashboard/consultant/rejectCandidate/{id}', name: 'app_dashboard_consultant_reject_offer', methods: ['GET'], requirements: ['id' => '\d+'])]
+    public function rejectOffer(JobOfferRepository $jobOfferRepository, int $id, EntityManagerInterface $entityManager): Response
+    {
+        $jobOffer = $jobOfferRepository->find($id);
+
+        $jobOffer->setIsValidated(false);
+        $entityManager->persist($jobOffer);
+        $entityManager->flush();
+
+        $this->addFlash(
+            'error',
+            'L\'annonce a été rejetée.'
+        );
+
+        return $this->redirectToRoute('app_dashboard_consultant_index');
     }
 }
